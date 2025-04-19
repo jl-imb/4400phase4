cfb33bf82eb670d1716189de9d68b18981141092
jl-imb <146906702+jl-imb@users.noreply.github.com>
2025-04-19 01:03:39 -0400

    more backend

--- db/db.py
+++ db/db.py
@@ -13,7 +13,7 @@ def get_connection():
 def test_connection():
     try:
         conn = get_connection()
-        cursor = conn.cursor()
+        cursor = conn.cursor(dictionary=True)
         return conn, cursor
     except Exception as e:
         print(f"DB Error: {str(e)}")
@@ -34,12 +34,33 @@ def add_airplane(airplaneID, tailnum, seatcap, speed, locID, type, maintenanced,
     if airplaneID == "" or tailnum == "" or seatcap == "" or speed == "" or locID == "" or type == "":
         return 0, "null vals"
     conn, cursor = test_connection()
-    cursor.callproc('add_airplane',
+    if maintenanced == "NULL" or maintenanced is None or maintenanced == "":
+        maintenanced = None
+    else:
+        maintenanced = str(maintenanced)
+    if model == "NULL" or model is None or model == "":
+        model = None
+    else:
+        model = str(model)
+    if neo == "NULL" or neo is None or neo == "":
+        neo = None
+    else:
+        neo = 1 if (neo == "True" or neo == True) else 0
+
+    try:
+        cursor.callproc('add_airplane',
                    [(str(airplaneID)), (str(tailnum)),(str(seatcap)), (str(speed)), (str(locID)),
-                   (str(type)), (str(maintenanced)), (str(model)), (str(neo))])
-    result = next(cursor.stored_results()).fetchone
-    status = result['status']
+                   (str(type)), maintenanced, model, str(neo)])
+    except Exception as e:
+        print("hi")
+        return 0, (f"Error: {str(e)}")
+
+    result = next(cursor.stored_results()).fetchone()
+    print("debug" + str(result))
+    status = result['status_']
     reason = result['reason']
+    conn.commit()
+    close_connection(conn, cursor)
     return status, reason
 
 
