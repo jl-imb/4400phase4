0929ba86f2b8495dda2f55966688a9d37d0c1b37
jojobaest <joiewqyeung@gmail.com>
2025-04-22 00:47:43 -0400

    frontend for add airplane

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/add_airplane', methods=['GET', 'POST'])
+def add_airplane_page():
+    if request.method == 'POST':
+        airlineID = request.form["airlineID"]
+        tailnum = request.form["tailnum"]
+        seatcap = request.form["seatcap"]
+        speed = request.form["speed"]
+        locID = request.form["locID"]
+        type = request.form["type"]
+        maintained = request.form["maintained"]
+        model = request.form["model"]
+        neo = request.form["neo"]
+        status, reason = add_airplane(airlineID, tailnum, seatcap, speed, locID, type, maintained, model, neo)
+        print(reason)
+        return render_template("add_airplane.html", reason=reason)
+    else:
+        return render_template("add_airplane.html")
+
 @app.route('/choose-table', methods=['POST'])
 def showdb():  # put application's code here
     if request.method == 'POST':
